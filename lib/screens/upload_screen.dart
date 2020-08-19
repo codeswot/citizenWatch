42f02de0dc8a88a7f4d5ae870c44668d5b6402c3
Mubarak Ibrahim <elmubarak333@gmail.com>
2020-08-19 22:51:24 +0100

    upload

--- lib/screens/upload_screen.dart
+++ lib/screens/upload_screen.dart
@@ -1,17 +1,154 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:ctz_wtch/widgets/button.dart';
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+// class UploadScreen extends StatefulWidget {
+//   static const String id = 'UploadScreen';
+//   @override
+//   _UploadScreenState createState() => _UploadScreenState();
+// }
+
+// class _UploadScreenState extends State<UploadScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [],
+//       ),
+//     );
+//   }
+// }
 class UploadScreen extends StatefulWidget {
-  static const String id = 'UploadScreen';
+  static const String id = 'UploadDiscussion';
   @override
   _UploadScreenState createState() => _UploadScreenState();
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  File _image;
+  final _picker = ImagePicker();
+  getImageFile(ImageSource source) async {
+    //Clicking or Picking from Gallery
+
+    var image = await _picker.getImage(source: source);
+
+    //Cropping the image
+
+    File croppedFile = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      maxWidth: 512,
+      maxHeight: 512,
+    );
+
+    setState(() {
+      _image = croppedFile;
+      print(_image.lengthSync());
+      print('test');
+    });
+  }
+
+  String dropdownValue;
   @override
   Widget build(BuildContext context) {
+    String defaultFontFamily = 'Roboto-Light.ttf';
+    double defaultFontSize = 14;
+    double defaultIconSize = 17;
     return Scaffold(
-      body: Column(
-        children: [],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 3.0,
+        titleSpacing: -3.0,
+        title: Text(
+          'Upload Post',
+          style: TextStyle(
+              color: Colors.grey,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Montserrat'),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(15.0),
+        padding: EdgeInsets.all(25.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 5.0,
+              offset: Offset(0.0, 2.5),
+            ),
+          ],
+        ),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: NetworkImage('images/logo.png'),
+                      radius: 30,
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      'Musa Damu',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16.5),
+              TextField(
+                maxLines: 5,
+                minLines: 2,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                showCursor: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  filled: true,
+                  prefixIcon: Icon(
+                    Icons.mail,
+                    color: Color(0xFF666666),
+                    size: defaultIconSize,
+                  ),
+                  fillColor: Color(0xFFF2F3F5),
+                  hintStyle: TextStyle(
+                      color: Color(0xFF666666),
+                      fontFamily: defaultFontFamily,
+                      fontSize: defaultFontSize),
+                  hintText: "What Happening ?",
+                ),
+                onChanged: (value) {
+                  // email = value;
+                },
+              ),
+              AuthButton(
+                name: 'UPLOAD',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
